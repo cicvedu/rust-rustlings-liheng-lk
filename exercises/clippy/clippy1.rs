334d14ee980b6cb52b1967fec0f61fa3ce215293
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2023-08-06 00:02:56 +0800

    Update clippy1.rs

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -19,8 +19,5 @@ fn main() {
 
     let area = pi * f32::powi(radius, 2);
 
-    println!(
-        "The area of a circle with radius {:.2} is {:.5}!",
-        radius, area
-    )
+    println!("The area of a circle with radius {:.2} is {:.5}!",radius, area);
 }
